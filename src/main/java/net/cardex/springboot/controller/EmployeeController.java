dc6e1be62264cbdfb33650b5e1b22fac45de3a3b
Amine Baba <ababa@rbbn.com>
2021-12-07 20:39:55 -0500

    initial commit

--- src/main/java/net/cardex/springboot/controller/EmployeeController.java
+++ src/main/java/net/cardex/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package net.cardex.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.cardex.springboot.model.Employee;
 import net.cardex.springboot.repository.EmployeeRepository;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
